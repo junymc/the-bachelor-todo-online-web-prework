15214668b56470b249732b10afb05c7cf1a0b75d
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-27 19:37:56 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,7 +55,7 @@ def get_average_age_for_season(data, season)
     age << season_hash["age"]
         number_of_age += season_hash["age"].to_i
   end
-  average = (number_of_age / age.size).round
+  average = (number_of_age.to_f / age.size).round
   average
   binding.pry
 end
